3513306f57f950ecf598755300e601426174ebd5
Vlad Cociorva <cocivlad@gmail.com>
2025-02-15 23:11:08 +0200

    Set seeds for reproducibility

--- src/train.py
+++ src/train.py
@@ -1,26 +1,34 @@
 import torch
 import numpy as np
+import random
 from model import GPT, GPTConfig
 from data import TokenShardDataloader
 
 GPT2_EOT = 50256
 
+seed = 42
+torch.manual_seed(seed)
+np.random.seed(seed)
+random.seed(seed)
+
 device = "cpu"
 if torch.cuda.is_available():
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
     device = "cuda"
 
 config = GPTConfig()
 model = GPT(config)
 model.to(device)
 
-steps = 1000
+steps = 5
 
 data_loader = TokenShardDataloader(
-    shards_dir="./tiny_shakespeare",
     B=8,
     T=1024,
     token_dtype=np.uint16,
     pad_value=GPT2_EOT,
+    shards_dir="./tiny_shakespeare",
 )
 
 x, y = data_loader.get_batch()
@@ -32,4 +40,4 @@ for step in range(steps):
     logits, loss = model(x, y)
     loss.backward()
     optim.step()
-    print(loss.item())
+    print(f'{loss.item():.6f}')
